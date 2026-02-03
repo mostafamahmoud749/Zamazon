25c8098761250e2cce30de6be59a015522cf5986
mostafamahmoud749 <yoyo2003266@gmail.com>
2026-02-03 23:29:46 +0200

    ,

--- app/(main)/cart/layout.tsx
+++ app/(main)/cart/layout.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode} from 'react';
 
 
-export default function CartLayout({children}:{children:ReactNode}):ReactNode{
+export default function CartLayout({children}:{children:ReactNode}){
     return children
 }
