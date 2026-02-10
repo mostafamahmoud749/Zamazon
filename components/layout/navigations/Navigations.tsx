cacf324e838d9ca109194e589108620e102316ea
mostafa mahmoud mahmoud ibrahim <yoyo2003266@gmail.com>
2026-02-10 05:49:09 +0200

    fixed typo

--- components/layout/navigations/Navigations.tsx
+++ components/layout/navigations/Navigations.tsx
@@ -1,21 +1,21 @@
 import Link from 'next/link';
 import { JSX } from 'react';
-import type {NavItem} from "@/types/index"
+import type { NavItem } from '@/types/index';
 
 type NavigationsProps = {
-  my:boolean
-  H:boolean
-  navs:NavItem[]
-  toggle:()=>void
-}
+  my: boolean;
+  H: boolean;
+  navs: NavItem[];
+  toggle: () => void;
+};
 
-export default function Navigations({ my, H, navs = [], toggle }:Partial<NavigationsProps>) {
-  const showNavs:JSX.Element[] = navs.map((el) => (
+export default function Navigations({ my, H, navs = [], toggle }: Partial<NavigationsProps>) {
+  const showNavs: JSX.Element[] = navs.map((el) => (
     <Link
       key={el.key}
       onClick={toggle}
       href={`/deals?filters=${encodeURIComponent(JSON.stringify(el))}`}
-      className={`${my ? 'my-4 ' : ''} transition-all duration-200 block ${H?"":"hover:ml-1"} pl-4 text-sm ${H ? 'flex-shrink-0 first:pl-0' : 'w-full'}`}
+      className={`${my ? 'my-4' : ''} block transition-all duration-200 ${H ? '' : 'hover:ml-1'} pl-4 text-sm ${H ? 'flex-shrink-0 first:pl-0' : 'w-full'}`}
     >
       {el.key}
     </Link>
@@ -23,16 +23,15 @@ export default function Navigations({ my, H, navs = [], toggle }:Partial<Navigat
 
   return (
     <div
-
       className={`p-1 ${
         H ? 'scrollbar-hide flex w-full max-w-full gap-3 overflow-x-auto scroll-smooth' : ''
       }`}
     >
       <Link
         href="/deals"
-        className={`${my ? 'my-4 ' : ''}  transition-all duration-200 block pl-4 text-sm ${H ? 'flex-shrink-0 first:pl-0' : 'w-full hover:ml-1'}`}
+        className={`${my ? 'my-4' : ''} block pl-4 text-sm transition-all duration-200 ${H ? 'flex-shrink-0 first:pl-0' : 'w-full hover:ml-1'}`}
       >
-        Today's Deals
+        Todays Deals
       </Link>
       {showNavs}
     </div>
