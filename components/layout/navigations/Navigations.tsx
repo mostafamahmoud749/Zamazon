1c3a61c60fd69efd7a330a676add9f9f00e43996
mostafa mahmoud mahmoud ibrahim <yoyo2003266@gmail.com>
2026-02-11 09:53:44 +0200

    added a button to show more or less navs in the sidebar

--- components/layout/navigations/Navigations.tsx
+++ components/layout/navigations/Navigations.tsx
@@ -1,6 +1,8 @@
+'use client';
 import Link from 'next/link';
 import { JSX } from 'react';
 import type { NavItem } from '@/types/index';
+import { useState } from 'react';
 
 type NavigationsProps = {
   py: boolean;
@@ -10,32 +12,44 @@ type NavigationsProps = {
 };
 
 export default function Navigations({ py, H, navs = [], toggle }: Partial<NavigationsProps>) {
+  const [showMore, setShowMore] = useState(false);
   const showNavs: JSX.Element[] = navs.map((el) => (
     <Link
       key={el.key}
       onClick={toggle}
       href={`/deals?filters=${encodeURIComponent(JSON.stringify(el))}`}
-      className={`${py ? 'py-2' : ''} block transition-all hover:bg-gray-100 duration-200 ${H ? '' : 'hover:translate-x-1'} pl-4 text-sm ${H ? 'flex-shrink-0 first:pl-0' : 'w-full'}`}
+      className={`${py ? 'py-2' : ''} block transition-all duration-200  ${H ? '' : 'hover:translate-x-1 hover:bg-gray-100'} pl-4 text-sm ${H ? 'flex-shrink-0 first:pl-0' : 'w-full'}`}
     >
       {el.key}
     </Link>
   ));
 
+  function toggleShowMore() {
+    setShowMore((prev) => !prev);
+  }
+
   return (
-    <div
-      className={`p-1 ${
-        H
-          ? 'scrollbar-hide flex w-full hover:bg-gray-100 max-w-full gap-3 overflow-x-auto scroll-smooth'
-          : 'max-h-[70vh] overflow-y-auto'
-      }`}
-    >
-      <Link
-        href="/deals"
-        className={`${py ? 'py-2' : ''} block pl-4 text-sm transition-all duration-200 ${H ? 'flex-shrink-0 first:pl-0' : 'w-full hover:translate-x-1'}`}
+    <div className="">
+      <div
+        className={`p-1 ${
+          H
+            ? 'scrollbar-hide flex w-full max-w-full gap-3 overflow-x-auto scroll-smooth '
+            : `overflow-y-auto ${showMore ? 'max-h-[60vh]' : 'max-h-[20vh]'}`
+        }`}
       >
-        Todays Deals
-      </Link>
-      {showNavs}
+        <Link
+          href="/deals"
+          className={`${py ? 'py-2' : ''} block pl-4 text-sm transition-all duration-200 ${H ? 'flex-shrink-0 first:pl-0' : 'w-full hover:translate-x-1'}`}
+        >
+          Todays Deals
+        </Link>
+        {showNavs}
+      </div>
+      {!H ? (
+        <button onClick={toggleShowMore} className="mt-2 flex justify-end w-full my-2 px-4 text-sm text-blue-500 cursor-pointer hover:underline">
+          {showMore ? 'Show Less' : 'Show More'}
+        </button>
+      ) : null}
     </div>
   );
 }
